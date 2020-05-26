0e2178ccffa34d6596ac03db937191604f1c6281
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 21:34:41 -0500

    The list of countries and the dictionary of capitals have been printed. Also, it has been printed the second capital of South Africa

--- 02_list-dicts.py
+++ 02_list-dicts.py
@@ -1,4 +1,10 @@
 #Creating a list of the BRICS
 country=["Brazil","Rusia","India","China","South Africa"]
 #Creating a dictionary of BRICS's capitals
-capital={"Brazil":"Brasilia","Rusia":"Moscow","India":"New Delhi","China":"Beijing","South Africa": ["Pretoria","Cape Town", "Bloemfontein"]}
+capital={"Brazil":"Brasilia","Rusia":"Moscow","India":"New Delhi","China":"Beijing","South Africa":["Pretoria","Cape Town","Bloemfontein"]}
+#let's print the countries
+print(country)
+#let's print all the capitals
+print(capital)
+#let's print only the second capital of South Africa
+print(capital["South Africa"][1])
