ef35f5f726a744eaf721b98092a715f4091757d3
fxalban8 <francisco.alban@epn.edu.ec>
2020-05-25 21:19:45 -0500

    A dictionary of BRIC's capitals has been created

--- 02_list-dicts.py
+++ 02_list-dicts.py
@@ -1,2 +1,4 @@
 #Creating a list of the BRICS
 country=["Brazil","Rusia","India","China","South Africa"]
+#Creating a dictionary of BRICS's capitals
+capital={"Brazil":"Brasilia","Rusia":"Moscow","India":"New Delhi","China":"Beijing","South Africa": ["Pretoria","Cape Town", "Bloemfontein"]}
